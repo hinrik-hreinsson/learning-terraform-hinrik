27c99b1feeab07105fc1f291a4d732ca14b9e3be
Hinrik Már Hreinsson <hinrik.hreinsson@netapp.com>
2024-07-08 13:20:08 +0000

    Fix error

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data aws_vpc" "default" {
+data "aws_vpc" "default" {
   default = true 
 }
 
@@ -36,31 +36,31 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule""blog_http_in"{
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
+resource "aws_security_group_rule" "blog_http_in"{
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   aws_security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule""blog_https_in"{
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
+resource "aws_security_group_rule" "blog_https_in"{
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   aws_security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule""blog_everything_out"{
-  type = "egress""
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
+resource "aws_security_group_rule" "blog_everything_out"{
+  type        = "egress""
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   aws_security_group_id = aws_security_group.blog.id
